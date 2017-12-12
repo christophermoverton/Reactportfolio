42fb77d85da5bf840d9099189054ca332f397ce6
Christopher Overton <christophermoverton@gmail.com>
2017-12-12 10:15:03 -0600

    Save

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Port1comp from './port1component';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Port1comp/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
