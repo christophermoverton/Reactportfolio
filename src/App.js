39326f04ac01b411159684e5b6fd314c61a080cf
Christopher Overton <christophermoverton@gmail.com>
2017-12-14 20:39:19 -0600

    save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Port1comp from './port1component';
+import logo2 from './Col3a.png'
+//import Port1comp from './port1component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Port1comp/>
-
+      <div>
+     
+        <img id="BgImage" src={logo2} alt="None"/>
+      
+      <div id="TitleText" x="167.5" y="149.5"  width={250} height={200}>Christopher Overton</div>
+        
+      
+    
+      <div id="TitleText2" x="187.5" y="249.5"  width={250} height={200}>Full Stack Developer and Designer</div>
       </div>
     );
   }
